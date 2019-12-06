97069718e95dbe9286ccc02a76a64f9156bcdff9
Anton Efimenko <reloni@ya.ru>
2019-12-06 22:40:53 +0300

    Initial Commit

---
+++ HtmlDsl/HtmlDsl.h
@@ -0,0 +1,19 @@
+//
+//  HtmlDsl.h
+//  HtmlDsl
+//
+//  Created by Anton Efimenko on 06.12.2019.
+//  Copyright © 2019 Anton Efimenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HtmlDsl.
+FOUNDATION_EXPORT double HtmlDslVersionNumber;
+
+//! Project version string for HtmlDsl.
+FOUNDATION_EXPORT const unsigned char HtmlDslVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HtmlDsl/PublicHeader.h>
+
+
